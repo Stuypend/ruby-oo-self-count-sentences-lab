3f7f93399711482da1eeb4a4a43ffbdf5a472f71
stuypend <andre@stuypend.com>
2021-02-23 16:31:24 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,43 @@ require 'pry'
 
 class String
 
+  def initialize()
+
+  end
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    idx = 0
+    count = 0
+    #binding.pry
+    while( idx < self.length)
 
+      if(self[idx] == "!" || self[idx] == "." || self[idx] == "?")
+        # binding.pry
+        count += 1
+        while(self[idx] == "!" || self[idx] == "." ||self[idx] == "?")
+          idx += 1
+          #  binding.pry
+        end
+      end
+      idx += 1
+    end
+    #binding.pry
+    return count
   end
+
 end
+
+
+puts "one. two. three?".count_sentences
